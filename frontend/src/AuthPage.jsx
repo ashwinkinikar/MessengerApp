abf6d71ecad8cd53d1f48dd0b51e68b0b4fc4dbf
Education <Edu@Ashwins-MacBook-Pro.local>
2024-03-05 17:45:54 -0800

    first commit

---
+++ frontend/src/AuthPage.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import axios from "axios";
+
+const AuthPage = (props) => {
+  const [username, setUsername] = useState("");
+  const [secret, setSecret] = useState("");
+  const [email, setEmail] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+
+  const onLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/login", { username, secret })
+      .then((r) => props.onAuth({ ...r.data, secret }))
+      .catch((e) => console.log(JSON.stringify(e.response.data)));
+  };
+
+  const onSignup = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/signup", {
+        username,
+        secret,
+        email,
+        first_name,
+        last_name,
+      })
+      .then((r) => props.onAuth({ ...r.data, secret }))
+      .catch((e) => console.log(JSON.stringify(e.response.data)));
+  };
+
+  return (
+    <div className="auth-page">
+      <div className="auth-left">
+        <form onSubmit={onLogin} className="auth-form">
+          <div className="title">Login</div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            name="secret"
+            placeholder="Password"
+            onChange={(e) => setSecret(e.target.value)}
+          />
+          <button type="submit">LOG IN</button>
+        </form>
+      </div>
+      <div className="auth-right">
+        <form onSubmit={onSignup} className="auth-form">
+          <div className="title">Sign Up</div>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            name="secret"
+            placeholder="Password"
+            onChange={(e) => setSecret(e.target.value)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="text"
+            name="first_name"
+            placeholder="First name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            name="last_name"
+            placeholder="Last name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <button type="submit">SIGN UP</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
